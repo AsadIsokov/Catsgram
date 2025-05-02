24664df805039a1b36e697216791714860cebf87
Asad Isokov <isokov.asad@mail.ru>
2025-05-03 02:35:02 +0300

    Первая версия проекта

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.catsgram.model;
+
+import java.time.Instant;
+
+public class User {
+    private Long id;
+    private String username;
+
+    private String email;
+    private String password;
+    private Instant registrationDate;
+}
